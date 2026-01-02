69445504a5f88bd48d2de0c6b07afdd01afd6ee2
Joe Battistello <joebattistello@hotmail.com>
2026-01-02 14:31:35 -0500

    modified:   flake.lock 	modified:   flake.nix 	modified:   hosts/crucible-dev/default.nix 	modified:   modules/home/niri.nix 	modified:   modules/home/shell.nix 	modified:   modules/system/desktop.nix 	modified:   scripts/new-host

---
+++ hosts/crucible-dev/default.nix
@@ -0,0 +1,18 @@
+{ inputs, ... }:
+
+{
+  imports = [
+    ../../modules/system/base.nix
+    ../../modules/system/desktop.nix
+    # ../../modules/system/gaming.nix
+
+    # Hardware config for this host (generate on the machine):
+    ./hardware-configuration.nix
+
+    # Niri NixOS module
+    inputs.niri.nixosModules.niri
+  ];
+
+  networking.hostname = "crucible-dev";
+
+}
