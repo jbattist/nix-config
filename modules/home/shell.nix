b90d8974ceff4757750d48234d1c5012da9118f5
Joe Battistello <joebattistello@hotmail.com>
2026-01-02 16:42:43 -0500

    modified:   modules/home/shell.nix

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -17,4 +17,19 @@
     git
   ];
 
+# Ghostty
+xdg.configFile."ghostty/config".source = dotfiles + "/ghostty/config";
+
+# Starship
+xdg.configFile."starship.toml".source = dotfiles + "/starship/starship.toml";
+
+# Zsh (either replace .zshrc...)
+home.file.".zshrc".source = dotfiles + "/zsh/.zshrc";
+
+# ...or better: source your custom file without replacing .zshrc
+# programs.zsh.initExtra = ''
+#   source ${dotfiles}/zsh/custom.zsh
+# '';
+
+
 }
