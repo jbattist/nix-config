c1a747efe5614faf4e2a5206fe8ef4970beb1cd1
Joe Battistello <joebattistello@hotmail.com>
2026-01-05 16:41:59 -0500

    modified:   modules/system/niri.nix

--- modules/system/niri.nix
+++ modules/system/niri.nix
@@ -8,5 +8,17 @@
   environment.systemPackages = with pkgs; [
     fuzzel              # launcher (common in Niri examples) :contentReference[oaicite:5]{index=5}
     xwayland-satellite  # for X11 apps under Wayland (often needed) :contentReference[oaicite:6]{index=6}
+    quickshell 
+    gpu-screen-recorder 
+    brightnessctl
+    cliphist
+    matugen-git
+    cava
+    wlsunset
+    xdg-desktop-portal
+    python3
+    evolution-data-server
+    polkit-kde-agent
+    
   ];
 }
